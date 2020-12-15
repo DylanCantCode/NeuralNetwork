d8bd302889db502fef4d9a1ef97863c7adcb9955
Dylan Lea <lady5998@ox.ac.uk>
2020-12-15 12:10:51 +0000

    Activation function (ReLU) and Network class

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -5,17 +5,29 @@ X = [[1, 2, 3, 2.5],
     [2.0, 5.0,-1.0, 2.0],
     [-1.5, 2.7, 3.3, -0.8]]
 
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.outputs = np.maximum(0, inputs)
+
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
     def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
+        self.outputs = np.dot(inputs, self.weights) + self.biases
+
+class Network:
+    def __init__(self, activation_func, *layers):
+        self.layers = layers
+    def forward(self, inputs):
+        self.outputs = inputs
+        for layer in self.layers:
+            layer.forward(self.outputs)
+            self.outputs = layer.outputs
 
 layer1 = Layer_Dense(4, 5)
 layer2 = Layer_Dense(5, 2)
 
-layer1.forward(X)
-#print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+network = Network(layer1, layer2)
+network.forward(X)
+print(network.outputs)
